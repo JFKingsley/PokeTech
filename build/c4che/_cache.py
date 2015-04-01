7f7c50fb56ebb69bd9af1f09a7ed070890e228b9
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-04-01 22:01:14 +0100

    .gitignore is now working

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/Jonathan/pebble-dev/PebbleSDK-3.0-dp2/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'aplite']
